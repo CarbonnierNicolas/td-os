ee104dec5e0f21be9d52ce9cd1bcfba8f2a6ea3e
CarbonnierNicolas <nicolaspro.carbonnier@gmail.com>
2017-02-01 12:56:39 +0100

    exo4

--- TD1/exo4.c
+++ TD1/exo4.c
@@ -1,1 +1,26 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#define EOF -1
 
+int getchar(int fd){
+	
+	int tmp;
+	if(read(fd,&tmp,1)) return tmp;
+	return EOF;
+	
+//	autre facon de coder getchar :
+//	int c;
+//	return (read(fd,&c,1)?c:EOF;
+
+}
+
+
+int main(){
+	
+int fd;
+fd=open("fd",O_RDONLY);	
+getchar(fd);
+
+}
